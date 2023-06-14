aafd839f334a437e0bdbc2943afe05232404471f
Meital Zamir (Twito) <tw.meital@gmail.com>
2023-06-14 15:40:09 +0300

    first commit

---
+++ src/componants/Form.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSumbit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    // Clear input fields
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSumbit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(+e.target.value)}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
